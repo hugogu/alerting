c7a6f961404882aabe1f4a2694ca4c4cced4f88e
谷强强 <guqiangqiang@lattebank.com>
2019-05-16 21:29:25 +0800

    fix build in IDE

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType<KotlinCompile> {
