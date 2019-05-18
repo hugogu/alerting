e536d330249b0dc2994da21baf92bda7684c2e0d
谷强强 <guqiangqiang@lattebank.com>
2019-05-18 14:05:47 +0800

    Refine output format.

--- src/main/kotlin/com/airwallex/codechallenge/output/Writer.kt
+++ src/main/kotlin/com/airwallex/codechallenge/output/Writer.kt
@@ -1,11 +1,15 @@
 package com.airwallex.codechallenge.output
 
 import com.airwallex.codechallenge.common.Marshalling
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter.FixedSpaceIndenter
 
 class Writer {
     private val mapper = Marshalling.mapper
 
     fun write(obj: Any): String {
-        return mapper.writeValueAsString(obj)
+        return mapper
+            .writer(DefaultPrettyPrinter().withObjectIndenter(FixedSpaceIndenter.instance))
+            .writeValueAsString(obj)
     }
 }
